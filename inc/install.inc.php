a0b8fcac9859e0963c1623bb1ea54bffdc787997
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-12 17:12:25 +0100

    Update install.inc.php

--- inc/install.inc.php
+++ inc/install.inc.php
@@ -8,7 +8,7 @@
     <script src="./assets/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <section class="container-sm" style="max-width: 540px;">
+    <section class="container-sm" style="max-width: 540px;margin-top: 100px;">
       <form mathod="post">
         <div class="mb-3">
           <label for="inputFirstname" class="form-label">Vorname</label>
