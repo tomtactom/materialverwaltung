41d7fc3f46c73e99cdbc9ce2d1549c262d7cad82
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-19 18:27:48 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,9 +1,25 @@
 <?php
   require('./inc/config.inc.php');
 
-  if(isset($_POST['field']) && isset($_POST['id'])){
-    $editid = $_POST['id'];
-    $statement = $pdo->prepare("UPDATE `main` SET `pack_id` = ".$pack_id." WHERE `row_id` = ".$editid);
+  $statement = $pdo->prepare("DESCRIBE `main`");
+  $statement->execute();
+  $table_fields = $statement->fetchAll(PDO::FETCH_COLUMN);
+  print_r($table_fields);
+  die();
+
+
+  if(isset($_POST['table']) && isset($_POST['entry']) && isset($_POST['value']) && isset($_POST['id'])) {
+    if ($_POST['table'] != 'main' and $_POST['table'] != 'pack' and $_POST['table'] != 'product' and $_POST['table'] != 'section') {
+      die(0);
+    } else {
+      $table = $_POST['table'];
+    }
+
+    /*
+    $entry
+    $value
+    $id*/
+    //$statement = $pdo->prepare("UPDATE `main` SET `pack_id` = ".$pack_id." WHERE `row_id` = ".$editid);
 
      echo 1;
   } else {
