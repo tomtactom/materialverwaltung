ba51d3d3cb2ee3bbabb7d26f896ef14d391cfac6
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-19 19:00:34 +0100

    Update update.php

--- update.php
+++ update.php
@@ -3,9 +3,21 @@
 
   $statement = $pdo->prepare("DESCRIBE `main`");
   $statement->execute();
-  $table_fields = $statement->fetchAll(PDO::FETCH_COLUMN);
-  print_r($table_fields[0]);
-  die();
+  $table_main = $statement->fetchAll(PDO::FETCH_COLUMN);
+
+  $statement = $pdo->prepare("DESCRIBE `pack`");
+  $statement->execute();
+  $table_pack = $statement->fetchAll(PDO::FETCH_COLUMN);
+
+  $statement = $pdo->prepare("DESCRIBE `product`");
+  $statement->execute();
+  $table_product = $statement->fetchAll(PDO::FETCH_COLUMN);
+
+  $statement = $pdo->prepare("DESCRIBE `section`");
+  $statement->execute();
+  $table_section = $statement->fetchAll(PDO::FETCH_COLUMN);
+
+  $column_names = array_merge($table_main, $table_pack, $table_product, $table_section);
 
 
   if(isset($_POST['table']) && isset($_POST['entry']) && isset($_POST['value']) && isset($_POST['id'])) {
@@ -14,6 +26,7 @@
     } else {
       $table = $_POST['table'];
     }
+    if (!in_array($_POST['entry'], $column_names))
 
     /*
     $entry
