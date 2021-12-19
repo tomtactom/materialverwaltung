c0205096c55c22354e4a938d16f4858f333e0ce8
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-19 23:21:04 +0100

    Update table.js

--- assets/js/table.js
+++ assets/js/table.js
@@ -27,6 +27,8 @@ $(document).ready(function(){
   $(this).prev('.edit').show();
   //$(this).prev('.edit').text(value[1]);
   document.getElementById('pack_'+id).innerHTML = value[1];
+  console.log(value[0]);
+  console.log(value[1]);
 
 
   var response;
