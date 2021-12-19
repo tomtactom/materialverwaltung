fef31bb324d63c0e28def3d09011fb5eb8aa81ad
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-19 20:54:28 +0100

    Update table.js

--- assets/js/table.js
+++ assets/js/table.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
  // Show Input element
  $('.edit').click(function(){
   $('.txtedit').hide();
-  $(this).next('.txtedit').show().click()
+  $(this).next('.txtedit').show().focus();
+  $('.txtedit').click();
   $(this).hide();
  });
 
