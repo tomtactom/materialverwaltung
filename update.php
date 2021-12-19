bae0b6006ddb14a0bcc2fa90ecb20b95a84c5dff
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-19 21:14:25 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,6 +1,13 @@
+<?php   require('./inc/config.inc.php'); ?>
+<form method="post">TEMP
+  <input name="table" value="main">
+  <input name="entry" value="pack_id">
+  <input name="value" value="1">
+  <input name="id" value="1">
+  <input name="securitycode" value="<?php echo sha1($config["admin_cookie_hash"]); ?>">
+  <input type="submit">
+</form>
 <?php
-  require('./inc/config.inc.php');
-
   // Write all columns from table in array
   $statement = $pdo->prepare("DESCRIBE `main`");
   $statement->execute();
@@ -19,40 +26,52 @@
   $table_section = $statement->fetchAll(PDO::FETCH_COLUMN);
 
   $column_names = array_merge($table_main, $table_pack, $table_product, $table_section);
-
-  /*if(!isset($_POST['table']) || !isset($_POST['entry']) || !isset($_POST['value']) || !isset($_POST['id']) || !isset($_POST['securitycode'])) {
-    die("Es wurden nicht alle Werte übermittelt.");
-  }*/
-
-  if($_POST['securitycode'] != sha1($config["admin_cookie_hash"])) {
-    die("Falscher Securitycode.");
+/*
+  if(!isset($_POST['table'])) {
+    die("1");
   }
-
-  if ($_POST['table'] != 'main' && $_POST['table'] != 'pack' && $_POST['table'] != 'product' && $_POST['table'] != 'section') {
-    die("Falscher 'table' Wert.");
-  } else {
-    $table = $_POST['table'];
+  if(!isset($_POST['entry'])) {
+    die("2");
   }
-
-  if (!in_array($_POST['entry'], $column_names)) {
-    die("Die angegeben Spalte existiert nicht.");
-  } else {
-    $entry = $_POST['entry'];
+  if(!isset($_POST['value'])) {
+    die("3");
   }
-
-  $value = htmlspecialchars($_POST['value']);
-
-  if(!is_numeric($_POST['id'])) {
-    die("Fehlerhafte ID.");
-  } else {
-    $id = $_POST['id'];
+  if(!isset($_POST['id'])) {
+    die("4");
   }
-  $statement = $pdo->prepare("UPDATE `".$table."` SET `".$entry."` = ".$value." WHERE `row_id` = ".$id);
-  $result = $statement->execute();
-  if ($result == true) {
-   echo 1;
- } else {
-   die("Die Daten konnten nicht gespeichert werden.");
- }
+  if(!isset($_POST['securitycode'])) {
+    die("5");
+  }
+*/
+    if($_POST['securitycode'] != sha1($config["admin_cookie_hash"])) {
+      die("Fehler 0");
+    }
+
+    if ($_POST['table'] != 'main' && $_POST['table'] != 'pack' && $_POST['table'] != 'product' && $_POST['table'] != 'section') {
+      die("Fehler 1: ".$_POST['table']);
+    } else {
+      $table = $_POST['table'];
+    }
+
+    if (!in_array($_POST['entry'], $column_names)) {
+      die("Fehler 2");
+    } else {
+      $entry = $_POST['entry'];
+    }
+
+    $value = htmlspecialchars($_POST['value']);
+
+    if(!is_numeric($_POST['id'])) {
+      die("Fehler 3");
+    } else {
+      $id = $_POST['id'];
+    }
+    $statement = $pdo->prepare("UPDATE `".$table."` SET `".$entry."` = ".$value." WHERE `row_id` = ".$id);
+    $result = $statement->execute();
+    if ($result == true) {
+     echo 1;
+   } else {
+     die("Fehler 4");
+   }
   exit;
 ?>
