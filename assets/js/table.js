91cd56c2239386a7976afa18b4ea3df25591a197
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-19 20:41:13 +0100

    Update table.js

--- assets/js/table.js
+++ assets/js/table.js
@@ -33,15 +33,10 @@ $(document).ready(function(){
    type: 'post',
    data: { table:table, entry:entry, value:value, id:id, securitycode:securitycode },
    success:function(response){
-      if(response == "1"){
+      if(response == 1) {
          console.log('Erfolgreich gespeichert.');
-      }else{
+      } else {
          console.log("Nicht gespeichert.");
-         console.log(table);
-         console.log(entry);
-         console.log(value);
-         console.log(id);
-         console.log(securitycode);
       }
    }
   });
