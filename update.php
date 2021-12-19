97dc8ce0912c8359c5ce84c81e363a2dc79cb633
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-19 17:49:28 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,3 +1,13 @@
 <?php
   require('./inc/config.inc.php');
+
+  if(isset($_POST['field']) && isset($_POST['id'])){
+    $editid = $_POST['id'];
+    $statement = $pdo->prepare("UPDATE `main` SET `pack_id` = ".$pack_id." WHERE `row_id` = ".$editid);
+
+     echo 1;
+  } else {
+     echo 0;
+  }
+  exit;
 ?>
