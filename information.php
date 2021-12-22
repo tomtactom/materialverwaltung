2a93a9ada77fc8f06f906474b35a47a06b7e8f2f
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-22 11:56:58 +0100

    Create information.php

---
+++ information.php
@@ -0,0 +1,24 @@
+<form method="post" action="?packing_degree=1">
+  <input type="text" name="product_id">
+  <input type="submit">
+</form>
+<?php
+require('./inc/config.inc.php');
+if($_POST['securitycode'] != sha1($config["admin_cookie_hash"])) {
+  //die("Falscher Securitycode.");
+}
+
+if(isset($_GET['packing_degree']) && isset($_POST['product_id'])) {
+  if(!is_numeric($_POST['product_id'])) {
+    die("Fehler: Die product_id entspricht nicht den Vorgaben");
+  }
+  $statement = $pdo->prepare("SELECT `packing_degree` FROM `product` WHERE `product_id` = ".trim($_POST['product_id']));
+  $result = $statement->execute();
+  if ($result == true) {
+    echo $statement->fetch()['packing_degree'];
+    exit();
+  } else {
+    die("Fehler: Fehler mit der SQL Abfrage.");
+  }
+  exit();
+}
