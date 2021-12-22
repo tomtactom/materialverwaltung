c69bc65f0ec8a54d317c62ede2a340940ede175e
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-22 12:16:49 +0100

    Update table.js

--- assets/js/table.js
+++ assets/js/table.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
 
     // GET packing_degree from product_id
     if ("date" == $(this).attr('type')) {
-      var packing_degree
+      var packing_degree;
       $.ajax({
         url: 'information.php',
         type: 'post',
@@ -49,7 +49,7 @@ $(document).ready(function() {
           product_id: value,
           securitycode: securitycode
         },
-        success: packing_degree;
+        success: packing_degree
       });
       console.log("#################" + packing_degree);
     }
