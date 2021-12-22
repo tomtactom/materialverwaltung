6a697ec63df50d4674c57bc26375ce6c918d8267
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-22 12:01:01 +0100

    Update information.php

--- information.php
+++ information.php
@@ -1,24 +1,15 @@
-<form method="post" action="?packing_degree=1">
-  <input type="text" name="product_id">
-  <input type="submit">
-</form>
 <?php
 require('./inc/config.inc.php');
-if($_POST['securitycode'] != sha1($config["admin_cookie_hash"])) {
-  //die("Falscher Securitycode.");
-}
-
-if(isset($_GET['packing_degree']) && isset($_POST['product_id'])) {
-  if(!is_numeric($_POST['product_id'])) {
-    die("Fehler: Die product_id entspricht nicht den Vorgaben");
+if(isset($_POST['securitycode']) && $_POST['securitycode'] == sha1($config["admin_cookie_hash"])) {
+  if(isset($_GET['packing_degree']) && isset($_POST['product_id'])) {
+    if(is_numeric($_POST['product_id'])) {
+      $statement = $pdo->prepare("SELECT `packing_degree` FROM `product` WHERE `row_id` = ".trim($_POST['product_id']));
+      $result = $statement->execute();
+      if ($result == true) {
+        echo $statement->fetch()['packing_degree'];
+        exit();
+      }
+      exit();
+    }
   }
-  $statement = $pdo->prepare("SELECT `packing_degree` FROM `product` WHERE `row_id` = ".trim($_POST['product_id']));
-  $result = $statement->execute();
-  if ($result == true) {
-    echo $statement->fetch()['packing_degree'];
-    exit();
-  } else {
-    die("Fehler: Fehler mit der SQL Abfrage.");
-  }
-  exit();
 }
