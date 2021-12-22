b20923cc71cc3bcf0c8987cdcb19dcae680a411b
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-22 12:38:13 +0100

    Update information.php

--- information.php
+++ information.php
@@ -1,19 +1,18 @@
 <?php
-print_r($_POST);
-require('./inc/config.inc.php');
-if(isset($_POST['securitycode']) && $_POST['securitycode'] == sha1($config["admin_cookie_hash"])) {
+  require('./inc/config.inc.php');
+  if(isset($_POST['securitycode']) && $_POST['securitycode'] == sha1($config["admin_cookie_hash"])) {
 
-  // GET → packing_degree from product_id
-  if(isset($_GET['packing_degree']) && isset($_POST['product_id'])) {
-    if(is_numeric($_POST['product_id'])) {
-      $statement = $pdo->prepare("SELECT `packing_degree` FROM `product` WHERE `row_id` = ".trim($_POST['product_id']));
-      $result = $statement->execute();
-      if ($result == true) {
-        echo $statement->fetch()['packing_degree'];
+    // GET → packing_degree from product_id
+    if(isset($_GET['packing_degree']) && isset($_POST['product_id'])) {
+      if(is_numeric($_POST['product_id'])) {
+        $statement = $pdo->prepare("SELECT `packing_degree` FROM `product` WHERE `row_id` = ".trim($_POST['product_id']));
+        $result = $statement->execute();
+        if ($result == true) {
+          echo $statement->fetch()['packing_degree'];
+          exit();
+        }
         exit();
       }
-      exit();
     }
-  }
 
-}
+  }
