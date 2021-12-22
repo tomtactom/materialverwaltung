8f7f6e6bef8f8e308cdad7e2764bf29b099a4610
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-22 11:58:20 +0100

    Update information.php

--- information.php
+++ information.php
@@ -12,7 +12,7 @@ if(isset($_GET['packing_degree']) && isset($_POST['product_id'])) {
   if(!is_numeric($_POST['product_id'])) {
     die("Fehler: Die product_id entspricht nicht den Vorgaben");
   }
-  $statement = $pdo->prepare("SELECT `packing_degree` FROM `product` WHERE `product_id` = ".trim($_POST['product_id']));
+  $statement = $pdo->prepare("SELECT `packing_degree` FROM `product` WHERE `row_id` = ".trim($_POST['product_id']));
   $result = $statement->execute();
   if ($result == true) {
     echo $statement->fetch()['packing_degree'];
