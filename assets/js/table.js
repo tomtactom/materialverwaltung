17a10096d0bf91ff814cdcbe86512ccd388aa0ab
Tom <32821177+tomtactom@users.noreply.github.com>
2021-12-22 11:11:18 +0100

    Update table.js

--- assets/js/table.js
+++ assets/js/table.js
@@ -2,15 +2,9 @@
 
 $(document).ready(function() {
 
-  $('input').on('input', function() {
-    var c = this.selectionStart,
-      r = /[^a-z0-9]/gi,
-      v = $(this).val();
-    if (r.test(v)) {
-      $(this).val(v.replace(r, ''));
-      c--;
-    }
-    this.setSelectionRange(c, c);
+  $(".txtedit").alphanum({
+    allow: "0123456789",
+    allowUpper: false
   });
 
   // Show Input element
